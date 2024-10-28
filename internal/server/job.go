e022e78dc3e9b7a29898c0cb70cb99cee148929e
dnslin <i@dnsl.in>
2024-10-28 11:46:20 +0800

    feat: init project

---
+++ internal/server/job.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"azure-vm-backend/pkg/log"
+	"context"
+)
+
+type Job struct {
+	log *log.Logger
+}
+
+func NewJob(
+	log *log.Logger,
+) *Job {
+	return &Job{
+		log: log,
+	}
+}
+func (j *Job) Start(ctx context.Context) error {
+	// eg: kafka consumer
+	return nil
+}
+func (j *Job) Stop(ctx context.Context) error {
+	return nil
+}
